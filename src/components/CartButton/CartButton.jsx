6ee90fe3605c5f856a8a7131a793c4f3bfc207fd
Vinicius Zavitoski <odeayotunde@gmail.com>
2024-05-01 15:06:16 -0300

    first commit

---
+++ src/components/CartButton/CartButton.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+import './CartButton.css';
+import AppContext from '../../context/AppContext';
+
+function CartButton() {
+
+    const { cartItems, isCartVisible, setIsCartVisible } = useContext(AppContext);
+
+    return (
+        <button
+            type="button"
+            className="cart__button"
+            onClick={ () => setIsCartVisible(!isCartVisible) }
+        >
+            <AiOutlineShoppingCart />
+            { cartItems.length > 0 && <span className="cart-status">{cartItems.length}</span> }
+        </button>
+    );
+}
+
+export default CartButton;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from 'react';
+import { BsSearch } from 'react-icons/bs';
+
+import './SearchBar.css';
+import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
+
+function SearchBar() {
+
+  const { setProducts, setLoading } = useContext(AppContext);
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setLoading(false);
+    setSearchValue('');
+  };
+
+  return (
+    <form className="search-bar" onSubmit={handleSearch}>
+      <input
+        type="search"
+        value={searchValue}
+        placeholder="Buscar produtos"
+        className="search__input"
+        onChange={ ({ target }) => setSearchValue(target.value) }
+        required
+      />
+
+      <button type="submit" className="search__button">
+        <BsSearch />
+      </button>
+    </form>
+  );
+}
+
+export default SearchBar;
